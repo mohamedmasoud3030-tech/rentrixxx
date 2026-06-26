2fa9d1eacbdbdbc2c2e17d7fbd996de30c61c333
mohamedmasoud3030-tech <mohamedmasoud3030@gmail.com>
2026-06-26 06:53:04 +0400

    feat(components): add index

---
+++ rentrix-app/src/components/layout/index.ts
@@ -0,0 +1,5 @@
+export { PageLayout } from "./page-layout";
+export { PageHeader } from "./page-header";
+export { ListPage } from "./list-page";
+export { AppShell } from "./app-shell";
+export { AuthLayout } from "./auth-layout";
