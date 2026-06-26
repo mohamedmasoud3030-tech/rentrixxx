848ce3864b42e93045fc98036b24bc1b75765c31
mohamedmasoud3030-tech <mohamedmasoud3030@gmail.com>
2026-06-26 06:53:05 +0400

    feat(components): add index

---
+++ rentrix-app/src/components/index.ts
@@ -0,0 +1,4 @@
+export { EmptyState } from "./empty-state";
+export { LoadingScreen } from "./loading-screen";
+export { DataErrorScreen } from "./data-error-screen";
+export { ErrorBoundary } from "./error-boundary";
